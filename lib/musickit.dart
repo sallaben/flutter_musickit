cf204e78d7bc868b060e2c8941cbf48b042a7252
sallaben <steven.allaben@gmail.com>
2020-05-23 14:29:08 -0600

    clean up

--- lib/musickit.dart
+++ lib/musickit.dart
@@ -6,29 +6,19 @@ import 'package:flutter/services.dart';
 class Musickit {
   static const MethodChannel _channel = const MethodChannel('musickit');
 
-  static Future<String> get ping async {
-    final String result = await _channel.invokeMethod('ping');
-    return result;
-  }
-
   static Future<String> get appleMusicRequestPermission async {
-    final String result = await _channel.invokeMethod('appleMusicRequestPermission');
-    return result;
+    return await _channel.invokeMethod('appleMusicRequestPermission');
   }
 
   static Future<String> get appleMusicCheckIfDeviceCanPlayback async {
-    final String result = await _channel.invokeMethod('appleMusicCheckIfDeviceCanPlayback');
-    return result;
+    return await _channel.invokeMethod('appleMusicCheckIfDeviceCanPlayback');
   }
 
   static Future<String> fetchUserToken(String developerToken) async {
-    final String result = await _channel.invokeMethod('fetchUserToken', developerToken);
-    print("fetchUserToken " + result);
-    return result;
+    return await _channel.invokeMethod('fetchUserToken', developerToken);
   }
 
   static Future<String> appleMusicPlayTrackId(List ids) async {
-    final String result = await _channel.invokeMethod('appleMusicPlayTrackId', ids);
-    return result;
+    return await _channel.invokeMethod('appleMusicPlayTrackId', ids);
   }
 }
